cbea4b78d8617732e68c7162706a7e164ae74fa6
mere-human <9664141+mere-human@users.noreply.github.com>
2023-04-16 22:49:40 +0300

    Add sum formula

--- run.py
+++ run.py
@@ -14,20 +14,28 @@ def main():
     slog = result.stdout.decode()
     lines1 = slog.split('\n')
     lines2 = []
+    LINE_DATA_COMMITS = 0
+    LINE_DATA_AUTHOR = 1
+    LINE_DATA_EMAIL = 2
     for l in lines1:
         # Format:
         # number of commits | author | <email>
         m = re.search(r'^\s*(\d+)\s+(.+)\s+<(\S+)>$', l)
         if m:
-            lines2.append(m.groups())
+            lines2.append([int(m.group(1)), m.group(2), m.group(3)])
     # print(lines2)
     # Generate the output.
     workbook = xlsxwriter.Workbook('result.xlsx')
     worksheet = workbook.add_worksheet()
     for i, x in enumerate(lines2):
-        ret = worksheet.write_row(i, 0, x)
+        ret = worksheet.write_row(
+            row=i, col=0, data=[x[LINE_DATA_AUTHOR], x[LINE_DATA_EMAIL], x[LINE_DATA_COMMITS]])
         if ret != 0:
             raise RuntimeError(f'Failed to write XLSX row: {ret}')
+    # Add sum formula.
+    stats_row = len(lines2) + 1
+    worksheet.write_string(row=stats_row, col=0, string='Total:')
+    worksheet.write_formula(row=stats_row, col=2, formula=f'=SUM(C1:C{len(lines2)})')
     workbook.close()
 
 
