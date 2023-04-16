1954174f8d703b1b662ce370d5c65ddb754674eb
mere-human <9664141+mere-human@users.noreply.github.com>
2023-04-16 20:58:37 +0300

    Parse shortlog output

--- run.py
+++ run.py
@@ -1,12 +1,24 @@
 import subprocess
 import sys
 import os
+import re
 
 def main():
+    # Get the data
     extra = ['--since="12 months"']  # TODO: pass as args
     cmd = ['git', 'shortlog', '-esn'] + extra
     result = subprocess.run(cmd, capture_output=True)
-    print(result.stdout.decode())
+    # Process the data.
+    slog = result.stdout.decode()
+    lines1 = slog.split('\n')
+    lines2 = []
+    for l in lines1:
+        # Format:
+        # number of commits | author | <email>
+        m = re.search(r'^\s*(\d+)\s+(.+)\s+<(\S+)>$', l)
+        if m:
+            lines2.append(m.groups())
+    print(lines2)
 
 if __name__ == '__main__':
     main()
