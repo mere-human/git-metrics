4a004fe44c1d4df501aa1aa5248a0af6d030bfce
mere-human <9664141+mere-human@users.noreply.github.com>
2023-04-16 23:03:20 +0300

    Add group sum

--- run.py
+++ run.py
@@ -25,17 +25,32 @@ def main():
             lines2.append([int(m.group(1)), m.group(2), m.group(3)])
     # print(lines2)
     # Generate the output.
-    workbook = xlsxwriter.Workbook('result.xlsx')
+    email_pattern = 'gmail.com'  # TODO: pass as an arg
+    group_rows = []
+    output_name = 'result.xlsx'  # TODO: pass as an arg
+    workbook = xlsxwriter.Workbook(output_name)
     worksheet = workbook.add_worksheet()
     for i, x in enumerate(lines2):
         ret = worksheet.write_row(
             row=i, col=0, data=[x[LINE_DATA_AUTHOR], x[LINE_DATA_EMAIL], x[LINE_DATA_COMMITS]])
         if ret != 0:
             raise RuntimeError(f'Failed to write XLSX row: {ret}')
+        if x[LINE_DATA_EMAIL].endswith(email_pattern):
+            group_rows.append(i)
     # Add sum formula.
     stats_row = len(lines2) + 1
     worksheet.write_string(row=stats_row, col=0, string='Total:')
-    worksheet.write_formula(row=stats_row, col=2, formula=f'=SUM(C1:C{len(lines2)})')
+    worksheet.write_formula(row=stats_row, col=2,
+                            formula=f'=SUM(C1:C{len(lines2)})')
+    # Add group sum.
+    if email_pattern and group_rows:
+        stats_row += 1
+        worksheet.write_string(row=stats_row, col=0,
+                               string=f'Total ({email_pattern}):')
+        group_cells = ','.join(['C' + str(x) for x in group_rows])
+        worksheet.write_formula(row=stats_row, col=2,
+                                formula=f'=SUM({group_cells})')
+
     workbook.close()
 
 
