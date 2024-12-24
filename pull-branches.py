27b8629a55212ad476a50610ab5f651b78840e05
mere-human <9664141+mere-human@users.noreply.github.com>
2024-12-24 22:30:26 +0200

    Disable submodule.recurse before pulling branches
    Otherwise, submodule may be in an invalid state.

--- pull-branches.py
+++ pull-branches.py
@@ -12,6 +12,11 @@ def main():
         if line:
             branches.append(line)
 
+    # avoid submodule errors
+    # TODO: read previous value
+    res = subprocess.run(f"git config --local submodule.recurse false")
+    logging.debug(f'{res}')
+
     logging.debug(f'Branches: {branches}')
 
     for branch in branches:
@@ -23,10 +28,15 @@ def main():
             subprocess.check_call(f'git fetch origin {branch}')
             subprocess.check_call(f'git checkout origin/{branch} -b {branch}')
         else:
-            logging.debug(f"Checking out and pulling {branch}...")
+            logging.debug(f"Check out {branch}...")
             subprocess.check_call(f'git checkout {branch}')
+            logging.debug(f"Pull {branch}...")
+            subprocess.check_call(f'git restore .')
             subprocess.check_call(f'git pull')
 
+    # avoid submodule errors
+    res = subprocess.run(f"git config --local submodule.recurse true")
+    logging.debug(f'{res}')
 
 if __name__ == '__main__':
     main()
